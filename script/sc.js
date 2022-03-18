9ef2d6ca67c13f606fca0c71d52c7dc4cb1b9f3c
KrakenTbot <100514008+KrakenTbot@users.noreply.github.com>
2022-03-18 14:31:31 +0200

    Add files via upload

---
+++ script/sc.js
@@ -0,0 +1,16 @@
+VANTA.NET({
+  el: "#bd",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x0,
+  backgroundColor: 0xd1ff,
+  points: 18.00,
+  maxDistance: 23.00,
+  spacing: 17.00,
+  showDots: false
+})
